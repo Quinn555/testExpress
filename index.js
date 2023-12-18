690b10161b9acde42ec22bd705b58c391b57702b
”Quin555” <QUINTIN.WEST3507@STU.MTEC.EDU>
2023-12-18 09:26:25 -0700

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/colou?r', (req, res) => {
+    res.send(`\nhi!\n`)
+})
+
+app.get(/i*he*flys?$/, (req, res) => {
+    res.send(`\ntrue\n`)
+})
+
+app.get(/i*apple$/, (req, res) => {
+    res.send(`\nApply\n`)
+})
+
+app.route('bannana')
+    .get((req, res) => {
+        console.log('get request');
+    })
+    .put((req, res) => {
+        console.log('get put');
+    })
+    .delete((req, res) => {
+        console.log('get delete');
+    })
+
+app.use(express.static('stuff'))
+
+app.listen(port, () => {
+    console.log(`running port ${port}`);
+})
